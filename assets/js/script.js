e8160006dcfa1b23b73b1749e40c6e8c577911b0
giahck <gianluca.giacchetta@gmail.com>
2024-03-15 15:06:01 +0100

    upd

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,8 @@ let btnPlay = document.querySelector(".play"); // Take the play button
 let btnPause = document.querySelector(".pause"); // Take the pause button
 let btnPrev = document.querySelector(".prev"); // Take the switch button of the previous track
 let btnNext = document.querySelector(".next");
+let btnShuffle  = document.querySelector(".shuffle");
+let btnRepeat  = document.querySelector(".repeat");
 
 const url = "https://deezerdevs-deezer.p.rapidapi.com/";
 let playlist = [];
@@ -19,7 +21,7 @@ class Ceraca {
     const options = {
       method: "GET",
       headers: {
-        "X-RapidAPI-Key": "4f9a41ff10mshfd328554e7f8c94p1e18a9jsn5a1f52cec344",
+        "X-RapidAPI-Key": "dbc55fa0b3msh11db0998867bdf0p127d0ejsn427d844c1974",
         "X-RapidAPI-Host": "deezerdevs-deezer.p.rapidapi.com",
       },
     };
@@ -94,6 +96,7 @@ playerDestra=()=>{
 
 playerSinistra=(track)=>{/* modificare grandezze css */
     // console.log(track.contributors[0].name);
+    console.log(track);
     playerInfo.innerHTML = `<div class='leftPlayer d-flex'>
           <img src="${track.album.cover_small}" alt="imgCover">
           <div>
@@ -109,7 +112,7 @@ puschTrack=async(canzone)=>{
   let playerSongC = new Ceraca('track/'+canzone);
   await playerSongC.featchFunction();
  console.log(playerSongC.data);
-    playlist.push(playerSongC.data.preview);
+    playlist.push(playerSongC.data);
     console.log(playlist);
 }
 
@@ -120,6 +123,8 @@ class playerHiden{
     this.timeBar = document.getElementById("timeBar");
     this.counterPlaylist=0;
     this.clearinterval;
+    this.shuffle=false;
+    this.repeatSongt=0;
   }
   visibile(){
       playerContent.classList.remove('playerDisactive');
@@ -150,10 +155,11 @@ class playerHiden{
     this.intervallo();
   }
   switchPlay(){
-    playerSinistra();
+    console.log(this.counterPlaylist);
+    playerSinistra(playlist[this.counterPlaylist]);
     this.audio.pause();
-    audio.src = playlist[this.counterPlaylist];
-    audio.currentTime =0;
+    audio.src = playlist[this.counterPlaylist].preview;
+    audio.currentTime =22;
     this.btnPlayVisibile();
     this.audio.play();
     clearInterval(this.clearinterval);
@@ -167,10 +173,20 @@ class playerHiden{
         let audioLength = Math.round(this.audio.duration);
         this.progress.style.width = (audioTime * 100) / audioLength + '%';
         this.timeBar.innerText=Math.round(this.audio.currentTime);
-        // console.log(playlist.length+'   '+this.counterPlaylist);
-        if ((this.audio.currentTime===this.audio.duration) && (this.counterPlaylist<playlist.length-1)) {
+         console.log(playlist.length-1+'   '+this.counterPlaylist);
+         if((this.audio.currentTime===this.audio.duration) &&this.repeatSongt>0){
+          this.repeatSongt--;
+          this.shuffle=false;
+          this.switchPlay();
+        } else if ((this.audio.currentTime===this.audio.duration) &&this.shuffle===false&& (this.counterPlaylist<playlist.length-1)) {
           // console.log('entra'+playlist.length);
           this.counterPlaylist++;
+          this.switchPlay();
+        }else if(this.audio.currentTime===this.audio.duration&& this.shuffle===true&&this.counterPlaylist===playlist.length-1){
+          this.progress.style.width=0+'%';
+          this.timeBar.innerText=0+'/'+Math.round(this.audio.duration);
+          this.stopPlay();
+          this.counterPlaylist=0;
           this.switchPlay();
         }else if(this.audio.currentTime===this.audio.duration&&this.counterPlaylist>=playlist.length-1){
           this.progress.style.width=0+'%';
@@ -182,11 +198,12 @@ class playerHiden{
   
   playlistAutoPlay(data){
     this.currentTime=0;
+    this.counterPlaylist=0;
     clearInterval(this.clearinterval);
     playerSinistra(data);
     this.audio.pause();
     audio.src = data.preview;
-    audio.currentTime =0;
+    audio.currentTime =22;
     this.btnPlayVisibile();
     this.audio.play();
     this.intervallo();
@@ -208,10 +225,22 @@ btnNext.addEventListener("click", function(e) {
 
 });
 btnPrev.addEventListener("click", function(e) {
-  if (playlist.length-1<=play.counterPlaylist) {
+  console.log(playlist.length+'  '+play.counterPlaylist);
+  if (play.counterPlaylist>=1) {
     play.counterPlaylist--;
     play.switchPlay();
   }
+});
+btnShuffle.addEventListener("click", function(e) {
+  
+});
+btnRepeat.addEventListener("click", function(e) {
+  
+  console.log(play.shuffle);
+  play.shuffle===true?play.shuffle=false:play.shuffle=true;
+});
+btnRepeat.addEventListener('dblclick', function(e) {
+  play.repeatSongt++
 });
 async function playerGet(id) {
   playlist=[];
@@ -220,7 +249,7 @@ async function playerGet(id) {
   let playerSong = new Ceraca('track/'+id);/* ricerco la traccia */
    await playerSong.featchFunction();
   //  console.log(playerSong.data);
-    puschTrack(playerSong.data.id);
+    playlist.push(playerSong.data)
     play.visibile();
     play.playlistAutoPlay(playerSong.data);
 }
@@ -322,7 +351,7 @@ async function playerGet(id) {
     const options = {
         method: "GET",
         headers: {
-          "X-RapidAPI-Key": "4f9a41ff10mshfd328554e7f8c94p1e18a9jsn5a1f52cec344",
+          "X-RapidAPI-Key": "dbc55fa0b3msh11db0998867bdf0p127d0ejsn427d844c1974",
           "X-RapidAPI-Host": "deezerdevs-deezer.p.rapidapi.com",
         },
       };
@@ -336,16 +365,16 @@ async function playerGet(id) {
     } else {
     //   console.log('Fetch riuscita');
     //    console.log(data);
-   
+   //loop(data,id);
     
   
-       if (contatoreGenera<1) {
+      if (contatoreGenera<1) {
         // console.log(data);
         popolaMainAlbum(data)
         //  getMainAlbum(data.album.id); 
         contatoreGenera++;
          return genera(tipo);            
-      } /*  else   if (contatoreGenera<4) {
+      }  /*  else   if (contatoreGenera<4) {
         // getHomeAlbum(random);
         popolaHomeAlbum(data);
         contatoreGenera++;
@@ -365,32 +394,30 @@ async function playerGet(id) {
         popolaHomeArtists2(data);
         contatoreGenera++;
          return genera(tipo);            
-      }else  if (contatoreGenera<21) {
+      }else*/  if (contatoreGenera<3) {
         //getHomeTracks(data.tracks['data'][0].id);
          popolaHomeTracks(data.tracks['data'][0]);
         // console.log(data.tracks['data'][0].id);
         contatoreGenera++;
          return genera(tipo);            
-      }  */
+      }  
     } 
   }
 
-  async function loadAlbum(id) {
-    let xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function () {
-      if (this.readyState == 4 && this.status == 200) {
-        document.getElementById("mainSection").innerHTML = this.response;
-      }
-    };
-    xhttp.open("GET", "album.html", true);
-    xhttp.send();
-    let mainAlbum = new Ceraca('album/'+ id);
-    
-    await mainAlbum.featchFunction();
-    popolaAlbum(mainAlbum.data);
-  
-  
-  }
+async function loadAlbum(id) {
+  let xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("mainSection").innerHTML = this.response;
+    }
+  };
+  xhttp.open("GET", "album.html", true);
+  xhttp.send();
+  let mainAlbum = new Ceraca("album/" + id);
+
+  await mainAlbum.featchFunction();
+  popolaAlbum(mainAlbum.data);
+}
   
   async function popolaPaginaAlbum(id){
   
@@ -588,29 +615,117 @@ function popolaHomeTracks(data) {
                   </div>
     `;
   }
+
+
   async function popolaAlbum(data) {
     console.log(data);
     const topMainBarAlbum = document.getElementById("topMainBarAlbum");
-    topMainBarAlbum.innerHTML +=` <div class="col-3">
-    <img class="w-100"
-      src="${data.cover}"
-      alt="" />
-  </div>
-  <div class="col-9 row">
-    <div class="d-flex align-items-end">
-      <p class="text-light m-0">Album</p>
-    </div>
-    <div class="d-flex align-items-center">
-      <h1 class="text-light m-0 fw-bolder display-1">${data.title}</h1>
-    </div>
-    <div class="d-flex align-items-end">
-      <p class="text-light m-0 ">
-        ${data.artist.name} · ${data.release_date} · ${data.nb_tracks} songs, <span class="coloreTesto">${Math.floor(data.duration/60)}</span> minuti
-      </p>
-    </div>
-  </div>
-  `
+    const listHeader = document.getElementById("listHeader");
+    const appendiLista = document.getElementById("appendiLista");
+    const albumButtons = document.getElementById("albumButtons");
   
+    topMainBarAlbum.innerHTML += ` <div class="col-3">
+      <img class="w-100"
+        src="${data.cover}"
+        alt="" />
+    </div>
+    <div class="col-9 row">
+      <div class="d-flex align-items-end">
+        <p class="text-light m-0">Album</p>
+      </div>
+      <div class="d-flex align-items-center">
+        <h1 class="text-light m-0 fw-bolder display-1">${data.title}</h1>
+      </div>
+      <div class="d-flex align-items-end">
+        <p class="text-light m-0 ">
+          ${data.artist.name} · ${data.release_date} · ${
+      data.nb_tracks
+    } songs, <span class="text-white">${Math.floor(
+      data.duration / 60
+    )}</span> minuti
+        </p>
+      </div>
+    </div>
+    `;
+  
+    albumButtons.innerHTML += `<div class="col-4 d-flex justify-content-around align-items-center w-25">
+    <button class="btn btn-dark rounded-circle py-1 px-1"  id="playAlbumButton">
+      <svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" fill="currentColor"
+        class="bi bi-play-circle-fill text-success w-100" viewBox="0 0 16 16">
+        <path
+          d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M6.79 5.093A.5.5 0 0 0 6 5.5v5a.5.5 0 0 0 .79.407l3.5-2.5a.5.5 0 0 0 0-.814z" />
+      </svg>
+    </button>
+    <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor"
+      class="bi bi-heart text-light cursorPointer mx-2" viewBox="0 0 16 16">
+      <path
+        d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143q.09.083.176.171a3 3 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15" />
+  
+    </svg>
+    <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor"
+      class="bi bi-download text-light cursorPointer mx-3" viewBox="0 0 16 16">
+      <path
+        d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5" />
+      <path
+        d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708z" />
+    </svg>
+  
+    <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor"
+      class="bi bi-three-dots text-light cursorPointer" viewBox="0 0 16 16">
+      <path
+        d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3m5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3m5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3" />
+    </svg>
+  </div>`;
+  
+  const playAlbumButton = document.getElementById("playAlbumButton");
+  
+  playAlbumButton.addEventListener('click', () => {
+    puschTrack(`${data}`);
+  });
+  
+  
+    listHeader.innerHTML += `<div class="row d-flex justify-content-between">
+    <div class="col-3 text-center coloreTesto w-auto"># </div>
+    <div class="col-5 coloreTesto p-0">
+      TITOLO
+    </div>
+    <div class="col-2 text-center coloreTesto">
+      RIPRODUZIONI
+    </div>
+    <div class="col-1"></div>
+    <div class="col-1 text-center coloreTesto">
+      <i class="bi bi-clock"></i>
+    </div>
+  </div>`;
+  
+    for (let i = 0; i < data.nb_tracks; i++) {
+  
+      appendiLista.innerHTML += `
+  
+    <li class="col-12 text-white my-2">
+     <div class="row">
+       <div class="col-6">
+         <div class="row">
+          <a href="#" class="col-12 text-white" onclick="playerGet(${data.tracks.data[i].id})">
+            <h5>${data.tracks.data[i].title}</h5>
+          </a>
+          <a href="#" class="col-12 text-white">
+            <h6>${data.artist.name}</h6>
+          </a>
+         </div> 
+       </div>
+       <div class="col-6">
+         <div class="row">
+           <p class="col-6 text-center">${data.tracks.data[i].rank}</p>
+           <p class="col-6 text-end">${(
+             data.tracks.data[i].duration / 60
+           ).toFixed(2)}</p>
+         </div>
+        </div>
+      </div> 
+    </li>
+  `;
+    }
   }
 function showPage(page) {
   const mainSection = document.getElementById("mainSection");
